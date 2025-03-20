9fe945ff17304b3a9041241492d1b0babcd3d771
gabrielvsantoss <gabrielvs3131@gmail.com>
2025-03-20 14:25:07 -0300

    Exercicio 02 e 03 concluido

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Exercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio = 0, altura = 0;
+
+            Console.WriteLine("Qual o valor do raio em cm?");
+            raio = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Qual o valor da altura em cm?");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+           double volume = Math.PI * Math.Pow(raio, 2) * altura;
+ 
+            Console.WriteLine($"volume é de {volume}cm³");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercicio03/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercicio03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double kmInicial = 0, kmFinal = 0, consumoCombustivel = 0;
+
+
+            Console.WriteLine("Km inicial:");
+            kmInicial = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Km inicial:");
+            kmFinal = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Consumo combustivel: ");
+            consumoCombustivel = Convert.ToDouble(Console.ReadLine());
+
+            double distanciaPercorrida = kmFinal - kmInicial;
+
+            double combustivelGastoPorKm = distanciaPercorrida / consumoCombustivel;
+
+            Console.WriteLine($"Combustivel gasto por km foi de: {combustivelGastoPorKm}");
+            Console.ReadLine();
+        }
+    }
+}
