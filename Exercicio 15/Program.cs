73238c29af123c8b79a5fda87ee38e138da19ef3
gabrielvsantoss <gabrielvs3131@gmail.com>
2025-03-21 14:35:40 -0300

    Questão 16 concluida

--- Exercicio 15/Program.cs
+++ Exercicio 15/Program.cs
@@ -1,22 +1,49 @@
 ﻿
+using System.Runtime.InteropServices;
+
 namespace Exercicio_15
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Qual seu peso?");
-            double peso = Convert.ToDouble(Console.ReadLine());
+            while (true)
+            {
+                Console.WriteLine("Qual seu peso?");
+                double peso = Convert.ToDouble(Console.ReadLine());
 
 
-            Console.WriteLine("Qual sua altura em metros? (Utilize virgula para separar os numeros)");
-            double altura = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Qual sua altura em metros? (Utilize virgula para separar os numeros)");
+                double altura = Convert.ToDouble(Console.ReadLine());
+
+                double IMC = peso / Math.Pow(altura, 2);
+
+                if (IMC < 18.5)
+                {
+                    Console.WriteLine($"Voce esta abaixo do peso seu IMC esta em: {IMC.ToString("F2")}");
+                    Console.ReadLine();
+                }
+
+                else if (IMC >= 18.5 && IMC <= 25)
+                {
+                    Console.WriteLine($"Voce esta no peso ideal sei IMC esta em: {IMC.ToString("F2")}");
+                    Console.ReadLine();
+                }
+
+                else if (IMC < 30)
+                {
+                    Console.WriteLine($"Voce esta acima do peso seu IMC esta em: {IMC.ToString("F2")}");
+                    Console.ReadLine();
+                }
+
+                else
+                {
+                    Console.WriteLine($"Voce esta com obesidade seu IMC esta em: {IMC.ToString("F2")}");
+                    Console.ReadLine();
+                }
 
 
-            double IMC = peso / Math.Pow(altura, 2);
-
-            Console.WriteLine($"IMC: {IMC.ToString("F2")}");
-            Console.ReadLine();
+            }
         }
     }
 }
---
+++ Exercicio 16/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Exercicio_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Digite um numero");
+                int numero = Convert.ToInt32(Console.ReadLine());
+
+                bool par = false, impar = false;
+
+                if (numero % 2 == 0)
+                    par = true;
+
+                else
+                    impar = true;
+
+
+                if (par == true)
+                {
+                    Console.WriteLine($"O numero {numero} é par");
+                    Console.ReadLine();
+                }
+
+                else 
+                {
+                    Console.WriteLine($"O numero {numero} é impar");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
