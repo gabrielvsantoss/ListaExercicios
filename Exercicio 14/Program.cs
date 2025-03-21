c7ef0521e5a480a4b3dbb1cdbc0ffff962c2b79c
gabrielvsantoss <gabrielvs3131@gmail.com>
2025-03-21 14:14:12 -0300

    Questão 15 e Questão 14 concluidas

--- Exercicio 14/Program.cs
+++ Exercicio 14/Program.cs
@@ -4,10 +4,6 @@
     {
         static void Main(string[] args)
         {
-            bool numero1Maior = false, numero1SegundoMaior = false, numero1Menor = false;
-            bool numero2Maior = false, numero2SegundoMaior = false, numero2Menor = false;
-            bool numero3Maior = false, numero3SegundoMaior = false, numero3Menor = false;
-
             while (true)
             {
 
@@ -20,78 +16,18 @@
                 Console.WriteLine("Escreva um numero inteiro");
                 int numero3 = Convert.ToInt32(Console.ReadLine());
 
-                if (numero1 > numero2 && numero1 > numero3)
-                    numero1Maior = true;
+                int[] numeros = [numero1, numero2, numero3];
 
-                else if (numero1 < numero2 && numero1 < numero3)
-                    numero1Menor = true;
+                Array.Sort(numeros);
 
-                else
-                    numero1SegundoMaior = true;
+                Array.Reverse(numeros);
 
-
-
-                if (numero2 > numero1 && numero2 > numero3)
-                    numero2Maior = true;
-
-                else if (numero2 < numero1 && numero2 < numero3)
-                    numero2Menor = true;
-
-                else 
-                    numero2SegundoMaior = true;
-
-
-
-                if (numero3 > numero2 && numero3 > numero1)
-                    numero3Maior = true;
-
-                else if (numero3 < numero1 && numero3 < numero2)
-                    numero3Menor = true;
-
-                else
-                    numero3SegundoMaior = true;
-
-
-
-
-
-                if (numero1Maior == true && numero2SegundoMaior == true && numero3Menor == true)
+                for(int i = 0; i < numeros.Length; i ++)
                 {
-                    Console.WriteLine($"{numero1} {numero2} {numero3}");
-                    Console.ReadLine();
+                    Console.WriteLine(numeros[i]);
                 }
 
-                else if (numero2Maior == true && numero1SegundoMaior == true && numero3Menor == true)
-                {
-                    Console.WriteLine($"{numero2} {numero1} {numero3}");
-                    Console.ReadLine();
-                }
-
-                else if (numero3Maior == true && numero2SegundoMaior == true && numero1Menor == true)
-                {
-                    Console.WriteLine($"{numero3} {numero2} {numero1}");
-                    Console.ReadLine();
-                }
-
-
-                else if (numero1Maior == true && numero3SegundoMaior == true && numero2Menor == true)
-                {
-                    Console.WriteLine($"{numero1} {numero3} {numero2}");
-                    Console.ReadLine();
-                }
-
-
-                else if (numero2Maior == true && numero3SegundoMaior == true && numero1Menor == true)
-                {
-                    Console.WriteLine($"{numero2} {numero3} {numero1}");
-                    Console.ReadLine();
-                }
-
-                else if (numero3Maior == true && numero1SegundoMaior == true && numero2Menor == true)
-                {
-                    Console.WriteLine($"{numero3} {numero1} {numero2}");
-                    Console.ReadLine();
-                }
+                Console.ReadLine();
             }
 
         }
---
+++ Exercicio 15/Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Exercicio_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual seu peso?");
+            double peso = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("Qual sua altura em metros? (Utilize virgula para separar os numeros)");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+
+            double IMC = peso / Math.Pow(altura, 2);
+
+            Console.WriteLine($"IMC: {IMC.ToString("F2")}");
+            Console.ReadLine();
+        }
+    }
+}
